04ccdba67409d677a96e51ef6e96828cdb859924
ManishaDere <manu.pathare263@gmail.com>
2020-12-11 19:50:47 +0530

    new dep changes 04

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
-export const url = `${process.env.VUE_APP_SOCKET_HOST || 'http://localhost'}:${process.env.VUE_APP_SOCKET_PORT || '3000'}`
+// export const url = `${process.env.VUE_APP_SOCKET_HOST || 'http://localhost'}:${process.env.VUE_APP_SOCKET_PORT || '3000'}`
+export const url = 'reverent-babbage-6fc2e7.netlify.app';
 
 export const STORE_ACTIONS = {
     joinRoom: 'joinRoom',
